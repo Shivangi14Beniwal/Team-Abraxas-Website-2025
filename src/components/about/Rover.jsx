cd9842b6ef24571e7303597e89c07e04df699a05
Sivichoudhary <shivangibeniwal338@gmail.com>
2025-03-26 21:50:25 +0530

    remove comments

--- src/components/about/Rover.jsx
+++ src/components/about/Rover.jsx
@@ -22,7 +22,7 @@ const Rover = ({ isMobile }) => {
       <primitive
         object={rover.scene}
         scale={isMobile ? 2 : 1.5}
-        position={[0, -1, -2]} // {isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={[0, -1, -2]} 
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -33,21 +33,21 @@ const RoverCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
+
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
+
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
+
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
+
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
+
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
